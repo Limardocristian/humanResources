45308bbf2df7a0986c4a7e2118c5d1651646b9a2
GabrielLimardo <gabriellim3001@gmail.com>
2022-06-05 14:00:21 -0300

    main

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,6 @@ const validator = require('../middlewares/validator');
 router.get("/",  homeController.root);
 router.post("/login",validator.login, homeController.login );
 router.post("/register", validator.register, homeController.register );
-router.post("/testing", homeController.testing );
 // , validator.register
 
 
